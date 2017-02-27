db2f8c70585109721d0fd8bddb5a9ac71bca0131
Yannick Formaggio <yannick.formaggio@gmail.com>
2017-02-26 18:30:54 -0800

    code_cleanup

--- cx10_getctrlchan/cx10_getctrlchan.ino
+++ cx10_getctrlchan/cx10_getctrlchan.ino
@@ -20,25 +20,21 @@
 #define PIN_CSN 8   // chip select (for SPI)
 
 RF24 radio(PIN_CE, PIN_CSN);
-const uint8_t num_channels = 127;
+const uint8_t num_channels = 90;    // CX-10 do not use channels after 90.
 uint8_t flags[num_channels];
 const int numReps = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(250000);
   printf_begin();                  // needed for printDetails() function
   Serial.println(F("nRF24l01+ CX-10 Scanner"));
 
   // Setup and configure RF Radio
   radio.begin();
-  //radio.setChannel(channel);
-  //radio.disableCRC();
   radio.setAutoAck(false);
-//  radio.setRetries(0,0);
   radio.setDataRate(RF24_1MBPS);
-//  radio.setPALevel(RF24_PA_MAX);
-//  radio.setAddressWidth(3);         // CX-10 use 5 bytes addresses
+  radio.setPALevel(RF24_PA_MIN);
 
   // Standby mode
   radio.startListening();
@@ -51,46 +47,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-int rep_counter = numReps;
-while  (rep_counter--)//numbers of repeats of each full scan
-//   for(int i = 0; i < numReps; i++)
-  {
+  int rep_counter = numReps;
+  while  (rep_counter--){ //number of repetitions of each full scan
     int i = num_channels;
-    while (i--)//scan over defined channels 
-//    for(int j =0; j < num_channels; j++)
-    {
-//      Serial.println(i);
-//      Serial.println("zzzz");
+    while (i--) { //scan over defined channels
       radio.setChannel(i);
- 
-      
       radio.startListening();
       delayMicroseconds(500);
-      
- 
-      
-      if ( radio.testRPD() )// Test if signals over -60dB on current signal
-      {
+
+      if (radio.testRPD()) { // Test if signals over -60dB on current signal
         flags[i]++;
       }
       radio.stopListening();
     }
   }
- 
-//  int i = 0;
-//  while ( i < num_channels )
-//  {
-//    Serial.println(flags[i]);
-//    ++i;
-//  }
-
-  for (int k = 0; k < num_channels; k++)//output channels and its time of apperance serial port
-  {
-    if(!(flags[k] == 0))
-    {
-    Serial.println(String(k) + ": " + String(flags[k]));
+  for (int k = 0; k < num_channels; k++) { //output channels and its time of apperance serial port
+    if(!(flags[k] == 0)){
+      Serial.println(String(k) + ": " + String(flags[k]));
     }
-//      Serial.println(flags[k]);
-    }
+  }
   Serial.println("End of 1 Full Scan");
 }
